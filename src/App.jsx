f5a9f4bb91ea5b8f98fa46347b719483f3800441
Nick Korintus <nicholas.korintus@gmail.com>
2024-09-04 16:51:43 -0400

    fetching job listings data from local json, starting basic app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
-import './App.css'
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid2';
+
+import './App.css';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  const getData = () => {
+    fetch('../data.json', {
+      headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+    .then(response => {
+      console.log('response: ', response)
+      return response.json();
+    })
+    .then(data => {
+      console.log('data: ', data);
+      setJobs(data);
+    });
+  }
+
+  useEffect(() => {
+    getData()
+  },[]);
+
   return (
     <>
-      
+      <header className='header'></header>
+      <div className='body'>
+        {jobs.length > 0 ? jobs.map(job => 
+        <Grid container>
+         <Grid size='grow'>
+          <Card>{job.company}</Card>
+         </Grid>
+        </Grid>)
+        : null}
+      </div>
     </>
   )
 }
